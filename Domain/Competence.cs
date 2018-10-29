c00e45366321eb171ccfb8bee49a6ee22b2fde7f
Zelaiti Chaima <zelaitich@gmail.com>
2018-10-30 00:21:47 +0100

    first commit project squelette

---
+++ Domain/Competence.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Competence
+    {
+        public int idCompetence { get; set; }
+        public String competenceName { get; set; }
+        public int rank { get; set; }
+    }
+}
---
+++ Domain/Profile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Profile
+    {
+        public int idProfile { get; set; }
+        public String profileName { get; set; }
+    }
+}
